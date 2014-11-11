1bc8a000e95ec429466aba569bdc4f50efdbf2df
unknown <hinamdar@MRC-5595.cscdom.csc.mrc.ac.uk>
2014-11-11 15:12:18 +0000

    initial_commit

--- RNASeqPipe.pl
+++ RNASeqPipe.pl
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use FileHandle;
 use Getopt::Long;
-
+use File::Basename;
 my $index;
 my $annotation_file;
 my $genome_fasta;
@@ -23,7 +23,10 @@ my $ref_genome_index_base = $index;
 my $gtf = $annotation_file;
 my $parent = $project_folder;
 my $strand_info = $stranded;
-my $DESeq_anl_fol = `mkdir $parent/../GeneCounts_for_DESeq`; 
+my $fullpath = $project_folder;
+my $base_dir = basename($fullpath);
+#print "file = $base_dir\n";
+my $DESeq_anl_fol = `mkdir $parent/../DESeq_$base_dir`; 
 my $par_dir;
 my $sub_dir;
 my $R1;
@@ -31,6 +34,7 @@ my $R2;
 my $full_path;
 my $outfile;
 my $ref_genome_fasta = $genome_fasta;
+my $count_folder;
 ##################################################################################################################
 ################## Read recursively through Project_Dir --> Sample_folder ---> fastq_files #######################
 ##################-------------------------------------------------------------------------#######################
@@ -48,26 +52,25 @@ while (my $sub_folders = readdir($par_dir)) {
            	$full_path = $path . '/' . $file;
           		if ($full_path=~/\_R1/) {
                 	$R1=$full_path;
-                }
+        }	
           		if ($full_path=~/\_R2/) {
                 	$R2=$full_path;
-                }
+    	}
 
-         }
+    }
          	close($sub_dir);
-
-       		### Now print list of commands in to be executed for each sample_folder into output file ==> scripts_out ####
+		$count_folder++;  ## counting no. of sample_folders
+       		### Now print list of commands to be executed for each sample_folder into output file ==> scripts_out ####
 		###------------------------------------------------------------------------------------------------------####
-	 	print $outfile "cd \$PBS_O_WORKDIR\n\n";
-         	print $outfile "module add tophat/2.0.8\n\n";
-         	print $outfile "module add bowtie2/2.1.0\n\n";
+	 	print $outfile "cd \$PBS_O_WORKDIR\n\n"; 
+         	print $outfile "module add tophat/2.0.8\n\n"; 
+         	print $outfile "module add bowtie2/2.1.0\n\n"; 
 	        print $outfile "module add samtools/0.1.18\n\n";
 		print $outfile "module add python/2.7.3\n\n";
          	print $outfile "module add htseq/0.5.4p3\n\n";
 	        print $outfile "tophat -p 8 -G $gtf -o $path/tophat_out_$sub_folders $ref_genome_index_base $R1 $R2\n\n";
 
-	 	### Can submit other jobs immeditely after tophat alignment : for ex; submitting a simeltaneous cufflinks job as shown below###
-		###-------------------------------------------------------------------------------------------------------------------------###
+	 	### Can submit other jobs immeditely after tophat alignment : for ex; submitting a simeltaneous cufflinks job as shown below :
 		#print $outfile "echo cufflinks -p 8 -N -G $gtf  -M \$DM3_MASK_GTF -b $ref_genome_fasta -o $path/tophat_out_$sub_folders/cufflinks_$sub_folders -u --compatible-hits-norm $path/tophat_out_$sub_folders/accepted_hits.bam| qsub -N cuff_$sub_folders -V -cwd \n\n";
 
 	 	print $outfile "samtools sort -n $path/tophat_out_$sub_folders/accepted_hits.bam $path/tophat_out_$sub_folders/sorted_bam_$sub_folders\n\n";
@@ -76,17 +79,36 @@ while (my $sub_folders = readdir($par_dir)) {
           	print $outfile "htseq-count -s $strand_info --order=name $path/tophat_out_$sub_folders/sorted_bam_$sub_folders.sam $gtf >$path/tophat_out_$sub_folders/$sub_folders.gene_count\n\n";
 	 	print $outfile "sed -i -e '1igene $sub_folders\' $path/tophat_out_$sub_folders/$sub_folders.gene_count\n\n";
 	 	print $outfile "awk -v OFS=\"\\t\" '\$1=\$1' $path/tophat_out_$sub_folders/$sub_folders.gene_count > $path/tophat_out_$sub_folders/$sub_folders.gene_counts\n\n";
-	 	print $outfile "mv $path/tophat_out_$sub_folders/$sub_folders.gene_counts $parent/../DESeq_anl_fol\n\n";
-	 
-   }
-     close($par_dir);
+	 	print $outfile "mv $path/tophat_out_$sub_folders/$sub_folders.gene_counts $parent/../DESeq_$base_dir\n\n";
+}  
+		close($outfile);
+
+my $count_lines;	
+open (my $infile,"<$scripts_out");
+while (<$infile>) {
+	chomp $_;
+	$count_lines++; ##counting no. of lines in scripts_out
+}
+my $split_count = $count_lines/$count_folder; ## 
+close($par_dir);
+
 #######################################################################################################################################################
 ################ Split the scripts_out file to create separate submission file for each sample_folder and then submit all of them simeltaneously ######
 ################---------------------------------------------------------------------------------------------------------------------------------######
-    my$split_command=`split -l 28  -d $scripts_out sub_job`;
-    my@split_array = glob "sub_job*";
+
+    my$split_command=`split -l $split_count  -d $scripts_out NEW_job`; ## split scripts_out to create separate submission file for each sample/replicate
+    my@split_array = glob "NEW_job*";
    #print "$split_array[0]\n";
-    foreach my$split_file(@split_array) {
-#   `qsub -l select=1:ncpus=1:mem=12GB -l walltime=70:00:00  $split_file`;
+   	 my $pattern = $base_dir;
+	 foreach my $split_file(@split_array) {
+	 open (my $splitfile,"<$split_file");
+	 while (my$splitline = <$splitfile>) {
+		if ($splitline =~/$pattern\/\S+\/$/){
+			print "...........................$1\n"; 
+			}
+			last;
+	 }
+	
+   `qsub -l select=1:ncpus=1:mem=12GB -l walltime=70:00:00  $split_file`;
    }
-     close($outfile);
+
